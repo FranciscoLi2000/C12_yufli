da7192678975acf031f0f89683041c272a8191b4
FranciscoLi2000 <francisco2000li@icloud.com>
2025-02-10 07:52:12 +0000

    find a random node of the list

---
+++ ex07/ft_list_at.c
@@ -0,0 +1,15 @@
+#include "ft_list.h"
+t_list	*ft_list_at(t_list *begin_list, unsigned int nbr)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (begin_list != NULL)
+	{
+		if (i == nbr)
+			return (begin_list);
+		begin_list = begin_list->next;
+		i++;
+	}
+	return (NULL);
+}
